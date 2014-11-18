cd3e538c7fec62cfa8740c941cfc7b7c300efe18
Simon Nistad <simon.nistad@bekk.no>
2014-11-18 22:00:41 +0100

    Testet med å legge inn Google Maps React component

--- js/components/Startpage.react.jsx
+++ js/components/Startpage.react.jsx
@@ -2,12 +2,14 @@ var _ = require("lodash");
 var React = require("react");
 
 var Logo = require('./Logo.react');
+var LocationMap = require('./LocationMap.react');
 
 module.exports = React.createClass({
     render: function() {
         return (
                 <div>
                     <Logo />
+                    <LocationMap />
                 </div>
             );
     }
