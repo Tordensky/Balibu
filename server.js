f4127f579498d2161386d9ffb3636160c42dcf95
Simon Nistad <simon.nistad@hotmail.com>
2015-03-22 12:20:52 +0100

    sender videre feilmelding fra værdata

--- server.js
+++ server.js
@@ -26,13 +26,14 @@ var WEATHER_URL = "http://api.openweathermap.org" +
     "/data/2.5/forecast/daily?lat=60.65785&lon=8.0269&units=metric&APPID=e63abeb43da0539704aa48fd21deeb6c";
 
 app.get('/api/weather', function (req, res) {
-    console.log("Got request for wheather in: ");
-
     request(WEATHER_URL, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body); // Show the HTML for the Google homepage.
             res.type('application/json')
                 .send(body)
+                .end();
+        } else {
+            res.status(response.statusCode)
+                .send(error)
                 .end();
         }
     });
@@ -42,4 +43,5 @@ var server = http.createServer(app);
 
 var port = process.env.PORT || 9999;
 app.listen(port);
+
 console.log("Server running on: localhost:", port);
